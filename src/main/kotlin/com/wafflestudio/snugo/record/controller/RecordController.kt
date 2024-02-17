8b1ee390c78258a854b5607c6c239a0afccda561
Chanyeong Lim <dlacksdud2@snu.ac.kr>
2024-02-18 02:18:51 +0900

    feat: add swagger description for routetype, record controller

--- src/main/kotlin/com/wafflestudio/snugo/record/controller/RecordController.kt
+++ src/main/kotlin/com/wafflestudio/snugo/record/controller/RecordController.kt
@@ -5,6 +5,7 @@ import com.wafflestudio.snugo.record.model.RankResponse
 import com.wafflestudio.snugo.record.model.RecordPageResponse
 import com.wafflestudio.snugo.record.model.RouteUploadRequest
 import com.wafflestudio.snugo.record.service.RecordService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
@@ -13,11 +14,13 @@ import org.springframework.web.bind.annotation.*
 class RecordController(
 	private val recordService: RecordService
 ) {
+	@Operation(summary = "기록 제출 api")
 	@PostMapping("/upload")
 	fun uploadRecord(@AuthenticationPrincipal authUserInfo: AuthUserInfo, @RequestBody routeUploadRequest: RouteUploadRequest) {
 		recordService.uploadRecord(authUserInfo, routeUploadRequest.route)
 	}
 
+	@Operation(summary = "나의 기록 보기, 최근순")
 	@GetMapping("/my")
 	fun getMyRecordList(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
@@ -27,6 +30,7 @@ class RecordController(
 		return recordService.getMyRecordList(authUserInfo.uid, page, size)
 	}
 
+	@Operation(summary = "전체 이용자의 최근 기록")
 	@GetMapping("/recent")
 	fun getRecentRecordList(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
@@ -36,6 +40,7 @@ class RecordController(
 		return recordService.getRecentRecordList(page, size)
 	}
 
+	@Operation(summary = "새로 세워진 신기록, 최근순")
 	@GetMapping("/newhigh")
 	fun getNewRecordList(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
@@ -45,6 +50,7 @@ class RecordController(
 		return recordService.getUpdatedHighScoreList(page, size)
 	}
 
+	@Operation(summary = "이 id를 갖는 경로(routeType)에 대해 시간 짧은 순")
 	@GetMapping("/top/{id}")
 	fun getTopWithRouteId(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
@@ -55,6 +61,7 @@ class RecordController(
 		return recordService.getTopWithRouteId(id, page, size)
 	}
 
+	@Operation(summary = "이 id를 갖는 경로(routeType)에 대해 최근 순")
 	@GetMapping("/recent/{id}")
 	fun getRecentWithRouteId(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
@@ -65,6 +72,7 @@ class RecordController(
 		return recordService.getRecentWithRouteId(id, page, size)
 	}
 
+	@Operation(summary = "이 id를 갖는 경로에 대해 나의 등수와 전체 기록 수")
 	@GetMapping("/myrank/{id}")
 	fun getMyRankForRouteType(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
@@ -73,6 +81,7 @@ class RecordController(
 		return recordService.getMyRank(id, authUserInfo.uid)
 	}
 
+	@Operation(summary = "전체 사용자 중 나의 위치(%), 나의 평균 등수 퍼센트")
 	@GetMapping("/myrank")
 	fun getMyRankForAll(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
--- src/main/kotlin/com/wafflestudio/snugo/record/controller/RouteTypeController.kt
+++ src/main/kotlin/com/wafflestudio/snugo/record/controller/RouteTypeController.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.snugo.record.controller
 import com.wafflestudio.snugo.common.auth.model.AuthUserInfo
 import com.wafflestudio.snugo.record.model.RouteTypePageResponse
 import com.wafflestudio.snugo.record.service.RouteTypeService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 class RouteTypeController(
 	private val routeTypeService: RouteTypeService
 ) {
+	@Operation(summary = "많은 사람들이 기록을 제출한 코스")
 	@GetMapping("/popular")
 	fun getPopularList(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
@@ -23,6 +25,7 @@ class RouteTypeController(
 		return routeTypeService.getPopularList(page, size)
 	}
 
+	@Operation(summary = "추천 코스, 200m 근방에서 출발하는 코스 인기 상위 10개")
 	@GetMapping("/recommend")
 	fun getRecommendList(
 		@AuthenticationPrincipal authUserInfo: AuthUserInfo,
