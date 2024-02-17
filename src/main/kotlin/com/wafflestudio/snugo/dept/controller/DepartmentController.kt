574e2831a63fadc7e27cdee01b041fa470fdd05a
hjlim7831 <hjlim7831@gmail.com>
2024-02-18 02:15:24 +0900

    Swagger API 설명 추가 (건물, 부서, 사용자)

--- src/main/kotlin/com/wafflestudio/snugo/dept/controller/DepartmentController.kt
+++ src/main/kotlin/com/wafflestudio/snugo/dept/controller/DepartmentController.kt
@@ -1,11 +1,15 @@
 package com.wafflestudio.snugo.dept.controller
 
 import com.wafflestudio.snugo.dept.model.Department
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DepartmentController {
+	@Operation(
+		summary = "단과대 리스트 조회 (회원가입 시 단과대 리스트 불러올 때 사용)"
+	)
 	@GetMapping("/v1/departments")
 	fun getDepartments(): List<String> {
 		return Department.entries.map { it.value }
